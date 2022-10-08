5f070162a81ac6598de593f2dc594d6471641fdd
josebernal12 <106898047+josebernal12@users.noreply.github.com>
2022-10-08 16:33:16 -0600

    Delete src directory

--- src/interfaces/car.interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface Car {
-    color: string
-    gas: 'gasoline ' | 'electric'
-    year: number;
-    description: string
-    price: number
-}
--- src/services/items.services.ts
+++
@@ -1,8 +0,0 @@
-import { Car } from "../interfaces/car.interfaces"
-import ItemModel from "../models/items"
-const insertItem = async (item: Car) => {
-    const response = await ItemModel.create(item)
-    return response
-}
-
-export { insertItem }
